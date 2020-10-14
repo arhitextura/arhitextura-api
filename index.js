d5110b5279d6aa17b1aea05efe25512a1f02911a
dani198912 <Feborcesim9>
2020-10-14 18:08:16 +0300

    cleaner code, more organzied and documented

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const api = express();
 const slackRoute = require('./routes/slack');
 const trelloRoute = require('./routes/trello')
 //Routes Middlewares
+// api.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "https://arhitextura.ro/"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+  
 api.use('/slack', slackRoute);
 api.use('/trello', trelloRoute)
 
--- routes/trello.js
+++ routes/trello.js
@@ -1,45 +1,68 @@
 const router = require("express").Router();
 const axios = require("axios");
-const {ERRORS_TYPE} = require('../constants/ERROR');
+const { ERRORS_TYPE } = require("../constants/ERROR");
 const trello = require("./trello_GET_functions.js");
 
 require("dotenv").config();
 
-
-
-//get all cards on the board
-router.get("/cards", async (req, res, next) => {
-  const resp = trello.getAllCardsOnBoard()
-  resp.then(data=> {
-    res.send(data)
-    return data
-  }).catch(error=>{
-    res.status(error.message).send("Error " + error.message + " " + ERRORS_TYPE[error.message].message)
-  })
+/**
+ * Get all cards on the board
+ * @example https://api.arhitextura.ro/trello/cards/
+ */
+router.get("/cards", async (req, res) => {
+  const resp = trello.getAllCardsOnBoard();
+  resp
+    .then((data) => {
+      res.send(data);
+      return data;
+    })
+    .catch((error) => {
+      res
+        .status(error.message)
+        .send(
+          "Error " + error.message + " " + ERRORS_TYPE[error.message].message
+        );
+    });
 });
 
-//Get all lists in the board
-router.get("/lists", async (req, res, next) => {
-  const resp = trello.getAllListsOnBoard()
-  resp.then(data=> {
-    res.send(data)
-    return data
-  }).catch(error=>{
-    res.status(error.message).send("Error " + error.message + " " + ERRORS_TYPE[error.message].message)
-  })
+/**
+ * Get all lists on the board
+ * @example https://api.arhitextura.ro/trello/cards/
+ */
+router.get("/lists", async (req, res) => {
+  const resp = trello.getAllListsOnBoard();
+  resp
+    .then((data) => {
+      res.send(data);
+      return data;
+    })
+    .catch((error) => {
+      res
+        .status(error.message)
+        .send(
+          "Error " + error.message + " " + ERRORS_TYPE[error.message].message
+        );
+    });
 });
 
-//Get all cards on a specified list
-router.get(`/cards/:id`, async (req, res, next) => {
-  const resp = trello.getCardsOnList(req.params.id)
-  resp.then(data=> {
-    res.send(data)
-    return data
-  }).catch(error=>{
-    res.status(error.message).send("Error " + error.message + " " + ERRORS_TYPE[error.message].message)
-  })
+/**
+ * Get all cards that are on a list specified by ID
+ * @example https://api.arhitextura.ro/trello/list/<#someId>/cards
+ */
+router.get(`/list/:id/cards`, async (req, res) => {
+  const resp = trello.getCardsOnList(req.params.id);
+  resp
+    .then((data) => {
+      res.send(data);
+      return data;
+    })
+    .catch((error) => {
+      res
+        .status(error.message)
+        .send(
+          "Error " + error.message + " " + ERRORS_TYPE[error.message].message
+        );
+    });
 });
-
-
 
 module.exports = router;
